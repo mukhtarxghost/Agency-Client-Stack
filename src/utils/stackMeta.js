0a8c91e1daabfd70b90d4f4864a50e1b1131f973
mukhtarxghost <mukhtarkarbelkar5@gmail.com>
2026-08-17 22:34:43 +0530

    Final

---
+++ src/utils/stackMeta.js
@@ -0,0 +1,13 @@
+export const STACK_NAME = "Agency Client Stack";
+
+export const STACK_TOOLS = [
+  "Invoice",
+  "Contract",
+  "Proposal",
+  "Welcome",
+];
+
+export const STACK_VERSION = "1.0.0";
+
+export const STACK_DESCRIPTION =
+  "A lightweight client-facing document toolkit for agencies and freelancers.";
